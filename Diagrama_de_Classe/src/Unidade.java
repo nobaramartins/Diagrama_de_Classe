c152f4801a20c9fdeb633d4d68638fc7dffa5091
Milena <martinsmilena086@gmail.com>
2025-09-19 18:54:53 -0500

    Getters e setters de unidade

--- Diagrama_de_Classe/src/Unidade.java
+++ Diagrama_de_Classe/src/Unidade.java
@@ -9,4 +9,30 @@ public class Unidade {
         this.nome = nome;
         this.endereco = endereco;
     }
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
