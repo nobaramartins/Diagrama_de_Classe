5f4b7517e3510a0ecfcb9dab6c35d35010043438
Milena <martinsmilena086@gmail.com>
2025-09-19 18:55:58 -0500

    Getters e setters de Profissional

--- Diagrama_de_Classe/src/Profissional.java
+++ Diagrama_de_Classe/src/Profissional.java
@@ -22,4 +22,70 @@ public class Profissional {
         this.especialidade = especialidade;
         this.usuario = usuario;
     }
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getRegistroConselho() {
+        return registroConselho;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Unidade getUnidade() {
+        return unidade;
+    }
+
+    public Especialidade getEspecialidade() {
+        return especialidade;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setRegistroConselho(String registroConselho) {
+        this.registroConselho = registroConselho;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUnidade(Unidade unidade) {
+        this.unidade = unidade;
+    }
+
+    public void setEspecialidade(Especialidade especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
