b885361a377806a10cada97ca9a6bc1e1059cec7
Milena <martinsmilena086@gmail.com>
2025-09-19 18:16:43 -0500

    Arquivos

---
+++ src/Especialidade.java
@@ -0,0 +1,10 @@
+public class Especialidade {
+    private Long id;
+    private String nome;
+
+    // Construtor
+    public Especialidade(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+}
